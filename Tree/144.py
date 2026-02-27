7621e31e5ffbce17751514335f2cb08831163990
yh167-sha <yachuanms@gmail.com>
2026-02-27 16:14:19 -0600

    Leetcode 144

---
+++ Tree/144.py
@@ -0,0 +1,48 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def preorderTraversal(self, root):
+        result = []
+        
+        def dfs(node):
+            if not node:
+                return
+             
+            #自己
+            result.append(node.val)
+            
+            # 左
+            dfs(node.left)
+            
+            # 右
+            dfs(node.right)
+        
+        dfs(root)
+        return result
+    
+def build_test_tree():
+    """
+          1
+           \
+            2
+           /
+          3
+
+    inorder 應該是 [1,3,2]
+    """
+    root = TreeNode(1)
+    root.right = TreeNode(2)
+    root.right.left = TreeNode(3)
+    return root
+
+
+if __name__ == "__main__":
+    root = build_test_tree()
+    sol = Solution()
+    print(sol.preorderTraversal(root))  # [1, 2, 3]
