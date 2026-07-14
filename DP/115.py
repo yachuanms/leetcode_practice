9bdee68f809d9aa6647f05bf54f6f3fe2095578b
yh167-sha <yachuanms@gmail.com>
2026-07-14 22:58:08 +0800

    Leetcode 115

--- DP/115.py
+++ DP/115.py
@@ -5,16 +5,34 @@ class Solution:
     def numDistinct(self, s: str, t: str) -> int:
 
         # lengths
+        lens = len(s)
+        lent = len(t)
 
         # define dp
+        DP = [[0] *(lent+1) for _ in range(lens+1)]
 
         # base case
+        for i in range(lens+1):
+            DP[i][0] = 1
+
+        #要避開DP[0][0]
+        for j in range(1, lent+1):
+            DP[0][j] = 0
 
         # fill dp
+        for i in range(1, lens+1):
+            for j in range(1, lent+1):
+                #最後一個字元相同:
+                #1. 留下
+                #2. 刪掉
+                if s[i-1] == t[j-1]:
+                    DP[i][j] = DP[i-1][j-1]+DP[i-1][j]
+                else:
+                    #刪掉
+                    DP[i][j] = DP[i-1][j]
 
         # return answer
-
-        pass
+        return DP[lens][lent]
 
 
 def main():
